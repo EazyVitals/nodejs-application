8df4e881d9363c712c65b7f6a89395cbadcc5bb8
Ibrahim Animashaun <iaanimashaun@gmail.com>
2023-07-12 19:27:17 +0100

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.40.145.89:9000/',
+  serverUrl: 'http://18.169.105.86:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
